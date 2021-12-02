ced57f62e514589dcf6f0b7cb2048f9fcac76810
derjr96 <54965311+derjr96@users.noreply.github.com>
2021-12-02 18:47:11 +0800

    clean up

--- djangotaurus/djangotaurus/admin.py
+++ djangotaurus/djangotaurus/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 from django.utils.translation import ugettext_lazy as _
 
-from .models import *
+from .models import User
+
 
 @admin.register(User)
 class UserAdmin(DjangoUserAdmin):
@@ -24,6 +25,7 @@ class UserAdmin(DjangoUserAdmin):
     list_display = ('email', 'first_name', 'last_name', 'is_staff')
     search_fields = ('email', 'first_name', 'last_name')
     ordering = ('email',)
+
 
 admin.site.register(Profile)
 admin.site.register(AccessToken)
--- djangotaurus/djangotaurus/settings.py
+++ djangotaurus/djangotaurus/settings.py
@@ -119,11 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'assets'),
-# )
-
 # Enable only for Linux Systems
 STATIC_ROOT = "/home/student127/staticfolder"
 
